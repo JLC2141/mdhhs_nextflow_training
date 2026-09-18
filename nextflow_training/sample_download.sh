08006ad2e129cc2f2ed582325fc4f557703e1439
JLC2141 <122481234+JLC2141@users.noreply.github.com>
2026-09-18 19:04:01 +0000

    changing sample

--- nextflow_training/sample_download.sh
+++ nextflow_training/sample_download.sh
@@ -3,14 +3,14 @@
 #Download FASTQ files of interest
 
 #Example pathogen from: https://www.ncbi.nlm.nih.gov/pathogens/pathogens_help/ SAMN05245394
-fasterq-dump SRR3747659
+fasterq-dump SRR39817210
 
 #gzip files
 gzip *.fastq
 
 #Rename files
-mv SRR3747659_1.fastq.gz SRR3747659_R1_001.fastq.gz
-mv SRR3747659_2.fastq.gz SRR3747659_R2_001.fastq.gz
+mv SRR39817210_1.fastq.gz SRR39817210_R1_001.fastq.gz
+mv SRR39817210_2.fastq.gz SRR39817210_R2_001.fastq.gz
 
 #move files to reads directory
 mkdir reads
